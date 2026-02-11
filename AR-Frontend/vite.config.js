e776e7ddcfcb39999799d64a9510eedd21dc40d0
Aboobacker F A <aboobackerfa580@gmail.com>
2026-02-11 08:49:33 +0530

    vite update

--- AR-Frontend/vite.config.js
+++ AR-Frontend/vite.config.js
@@ -1,21 +1,18 @@
-export default {
+import { resolve } from 'path';
+  import { defineConfig } from 'vite';
+  
+  export default defineConfig({
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          home: resolve(__dirname, 'home/index.html'),
+          page3: resolve(__dirname, 'page3/index.html'),
+          teamreg: resolve(__dirname, 'teamreg/index.html'),
+        },
+      },
+    },
     server: {
       port: 5173,
-      middleware: [
-        (req, res, next) => {
-          const path = req.url.split('/')[1];
-  
-          // Redirect to /{folder}/index.html
-          const redirectTo = `/home/index.html`;
-          res.writeHead(302, { Location: redirectTo });
-          if (path && path !== '' && path !== 'index.html') {
-            const redirectTo = `/${path}/index.html`;
-            res.writeHead(302, { Location: redirectTo });
-            res.end();
-          } else {
-            next();
-          }
-        },
-      ],
     },
-  };
+  });
